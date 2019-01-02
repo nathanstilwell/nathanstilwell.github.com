816f20f906bd52991405e44c063a1b970801d51e
Nathan Stilwell <nathan@flow.io>
2019-01-02 16:37:16 -0500

    archiving old site

--- archive/gulpfile.js
+++ archive/gulpfile.js
@@ -1,5 +1,18 @@
 'use strict';
 
+/**
+ * For reference only. Most likely, these dependencies have been taken out
+ * of the package.json and won't work any longer. In case of emergancy, here
+ * is what you would need.
+ *
+ *  "autoprefixer": "9.4.3",
+ *  "gulp": "4.0.0",
+ *  "gulp-postcss": "8.0.0",
+ *  "gulp-sourcemaps": "2.6.4",
+ *  "postcss-import": "12.0.1",
+ *  "postcss-nested": "4.1.1",
+ *  "postcss-simple-vars": "5.0.1"
+ */
 const { src, dest } = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('autoprefixer');

2433da2c9d3a30427b43298a8c0de09334ca45d3
Nathan Stilwell <nathan@flow.io>
2019-01-02 21:47:38 -0500

    Robot Website

---
+++ pages-manifest-612cc5eded7a3e99d864.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{135:function(e){e.exports={pages:[{componentChunkName:"component---src-pages-index-js",jsonName:"index",path:"/"},{componentChunkName:"component---src-pages-resume-js",jsonName:"resume-e4f",path:"/resume/"}],dataPaths:{index:"173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E","resume-e4f":"794/path---resume-e-4-f-cdf-NZuapzHg3X9TaN1iIixfv1W23E"}}}}]);
+//# sourceMappingURL=pages-manifest-612cc5eded7a3e99d864.js.map
--- src/pages/resume.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Resume = () => (
-  <main>
-    <h1># Nathan Stilwell</h1>
-  </main>
-);
-
-export default Resume;
